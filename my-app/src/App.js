3b2cffa869b70f5207843060da5273fbd73a0d47
Zorn89 <jan.zorn@tn.techstarter.de>
2025-04-16 14:11:06 +0200

    commit

---
+++ my-app/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Mein erster Versuch</h1>
+        <p>3 Gerichte</p>
+        <ul>
+          <li>Pizza</li>
+          <li>Döner</li>
+          <li>Currywurst</li>
+          
+        </ul>
+        <button>Drück mich</button>
+      </header>
+    </div>
+
+
+  );
+}
+
+export default App;
